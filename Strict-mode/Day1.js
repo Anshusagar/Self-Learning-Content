349461676f7f96e40437f121e4c660e2b66ad526
Anshu Sagar <yuvraaj.anshu@gmail.com>
2021-01-06 20:59:36 +0530

    Strict Mode

---
+++ Strict-mode/Day1.js
@@ -0,0 +1,8 @@
+'use strict';
+let hasDriver=false;
+const pass=true;
+if(pass) hasDriver=true;
+if(hasDriver)console.log("Paased")
+
+//Sometime it happens like you did not any error in console for that we Use A line 'use strict ' mode if something went error in our code
+//it will imedietely fetch that error and show the error
